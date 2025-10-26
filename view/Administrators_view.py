be931e6ff6ef73311d82e587ce92fea782b52c75
Ekaterina <ekaterinaanabekova@gmail.com>
2025-10-27 01:36:44 +0500

    исправление функции input_admin_data, добавление функции show_add_admin_result, добавление функций display_login_success, display_registration_success, display_register_failure, login_failure, display_error

--- view/Administrators_view.py
+++ view/Administrators_view.py
@@ -15,13 +15,32 @@ def input_admin_data()-> tuple[str, str]:
     """
     login_admin = input("Логин: ")
     password = input("пароль: ")
-    return login_admin, password
-def show_add_admin_result(success: bool):
+    phone = input("телефон: ")
+    return login_admin, password, phone
+def show_add_admin_result(admin_data):
     """
     Вывод сообщения об операции добавления пользователя администратор
     :param success:результат добавлния
     """
-    if success:
-        print("Пользователь администратор успешно добавлен")
-    else:
-        print("Ошибка при добавлении пользователя администратор")
+    print("\nВход успешно выполнен!")
+
+def display_login_success(admin_data):
+    print("\nВход успешно выполнен!")
+    print(f"Логин: {admin_data['login']}")
+    print(f"Пароль: {admin_data['password']}")
+    print(f"Телефон: {admin_data['phone']}")
+
+def display_registration_success(admin_data):
+    print("Регистрация прошла успешно!")
+    print(f"Логин: {admin_data['login']}")
+    
+    print(f"Телефон: {admin_data['phone']}")
+
+def display_register_failure(message: str = "Вход не выполнен. Администратор уже зарегистрирован"):
+    print(f"\n{message}")
+
+def login_failure(message: str = "Вход не выполнен. Неверный логин или пароль."):
+    print(f"\n{message}")
+
+def display_error(message: str = "Произошла неизвестная ошибка."): 
+    print(f"\nОшибка: {message}")
