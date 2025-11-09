ef2bc5fc8190ab1347cb6ab28df94a830a519921
Ekaterina <ekaterinaanabekova@gmail.com>
2025-11-10 00:31:40 +0500

    добавление функций для работы с базой данных

--- db/database.py
+++ db/database.py
@@ -1,7 +1,8 @@
-from sqlmodel import SQLModel, create_engine, Session
-import os
+from typing import Generator
+from sqlmodel import SQLModel, Session
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
-import psycopg2
+import os
 load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
@@ -9,14 +10,26 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DBNAME = os.getenv("DBNAME")
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DBNAME}"
+if not all([DB_USER, DB_PASS, DB_HOST, DB_PORT, DBNAME]):
+    raise ValueError("не все переменные Базы данных поключены")
 
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DBNAME}"
 engine = create_engine(DATABASE_URL, echo=True)
 def init_db():
-    print(DATABASE_URL)
+    print(f"инициализация Базы данных по URL:{DATABASE_URL}")
     SQLModel.metadata.create_all(engine)
-    
-def get_session():
+    print("Таблицы Базы данных созданы, если не существовали ")
+
+def close_db():
+    print("Закрытие соединения с Базой данных")
+    engine.dispose() # Освобождение ресурсов
+    print("Соединение с Базой данных закртыто")
+
+def get_engine():
+    """Получение engine для миграций"""
+    return engine
+
+def get_session()-> Generator[Session, None, None]:
     """
     Возвращает сессию базы данных. Используется FastAPI как зависимость.
     """
