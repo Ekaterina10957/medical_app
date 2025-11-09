604dece0828125bc8facc41ea47d8f0e871fde9f
Ekaterina <ekaterinaanabekova@gmail.com>
2025-11-10 00:29:16 +0500

    новый подход lifespan вместо устаревшего @app.on_event("startup").  добавление эндпоинтов для бухгалтера

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI, HTTPException, Depends
-from sqlmodel import Session # Убедись, что это 'from sqlmodel import Session'
-from model.LabAssistants import LabAssistants # Предполагаем, что файл называется lab_assistants.py в папке models
+from sqlmodel import Session 
+from model.LabAssistants import LabAssistants 
 from model.Administrators import Administrator
+from model.Accountants import Accountants
 from controller.LabAssistants_Controller import LabAssistants_Controller, get_password_hash, verify_password
 from controller.Administrators_controllers import Administrators_controllers,get_password_hash, verify_password
-from db.database import get_session, init_db
+from controller.Accountants_controller import Accountants_controller, get_password_hash, verify_password
+from db.database import close_db, get_session, init_db
+from schemas.Accountant import AccountantCreate, AccountantLogin, AccountantPublic
 from schemas.LabAssistant import LabAssistantCreate, LabAssistantLogin, LabAssistantPublic 
 from schemas.Administrators import AdministratorsPublic, AdministratorsCreate, AdministratorsLogin
 from datetime import datetime
+from contextlib import asynccontextmanager
 
-app = FastAPI()
 
-# --- Хук для инициализации БД при старте приложения ---
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def on_startup(app: FastAPI):
     init_db()
+    yield
+    close_db()
+app = FastAPI(lifespan=on_startup)
 
 @app.get("/")
 async def read_root():
@@ -69,3 +74,30 @@ async def login_admin_assistant_api(
         raise HTTPException(status_code=401, detail="Incorrect login or password.")
     updated_admin = Administrators_controllers.update_admin_last_login(session, admin_db)
     return updated_admin
+
+
+
+# Эндпоинт для регистрации бухгалтера 
+@app.post("/accountant/register",response_model= AccountantPublic, status_code= 200) 
+async def accountant_register_api(
+        accountant_data: AccountantCreate,
+        session: Session = Depends(get_session)
+):
+    existing_accountan = Accountants_controller.get_accountant_by_login_for_auth(session, accountant_data.login)
+    if existing_accountan:
+        raise HTTPException(status_code=400, detail="Login already registered.")
+    created_accountant = Accountants_controller.create_accountant(session, accountant_data)
+    return created_accountant
+
+
+#Эндпоинт для входа бухгалтера 
+@app.post("/accountant/login", response_model=AccountantPublic) 
+async def login_accountant_assistant_api(
+    credentials: AccountantLogin,
+    session: Session = Depends(get_session)
+):
+    accountant_db = Accountants_controller.get_accountant_by_login_for_auth(session, credentials.login)
+    if not accountant_db or not verify_password(credentials.password, accountant_db.hashed_password):
+        raise HTTPException(status_code=401, detail="Incorrect login or password.")
+    updated_accountant = Accountants_controller.update_accountant_last_login(session, accountant_db)
+    return updated_accountant
